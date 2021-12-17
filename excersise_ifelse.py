6b93b1c65af5ec0db647dedf1c9001fff29d605a
Parth Galoliya <96252142+gparth30@users.noreply.github.com>
2021-12-17 22:29:12 +0530

    Exercise of if_else decision making

---
+++ excersise_ifelse.py
@@ -0,0 +1,17 @@
+room1w=int(input("Enter Room1 Width"))
+room1l=int(input("Enter Room1 Length"))
+area1=room1w*room1l
+print(f"Area Of Room1={area1}")
+
+room2w=int(input("Enter Room2 Width"))
+room2l=int(input("Enter Room Length"))
+area2=room2w*room2l
+print(f"Area Of Room2={area2}")
+
+
+if area1>area2:
+    print("Area Of Room1 Is Largest Then Rroom2")
+
+else:
+    print("Area Of Room2 Is Largest Then Room1")
+print("good Bye....") 
