60d56f1083a73681f2c2a6ca2dd898f932cc449b
Parth Galoliya <96252142+gparth30@users.noreply.github.com>
2021-12-17 22:28:15 +0530

    Excersise of if decision

---
+++ excersise_if.py
@@ -0,0 +1,8 @@
+# Example of if age is above 21 then display message "you are eligible for marriage"
+age=int(input("Enter Your Age Above 21.:"))
+
+if age>21: 
+    print("You Are Eligible For Marraige")
+    
+
+print("Good Bye....")
