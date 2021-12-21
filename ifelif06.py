111dd4c5d8ecabec96d77a604732998fad1d5e11
Parth Galoliya <96252142+gparth30@users.noreply.github.com>
2021-12-21 15:29:12 +0530

    ifelif program for blood donate

---
+++ ifelif06.py
@@ -0,0 +1,131 @@
+donor=str(input("Enter Donor Blood group.:"))
+beneficiery=str(input("Enter Beneficiery Blood Group.:"))
+blood_group=donor+beneficiery
+
+if blood_group=="o-o-":
+    print("donor can give blood")
+elif blood_group=="o-o+":
+    print("donor can give blood")
+elif blood_group=="o-a-":
+    print("donor can give blood")
+elif blood_group=="o-a+":
+    print("donor can give blood")
+elif blood_group=="o-b-":
+    print("donor can give blood")
+elif blood_group=="o-b+":
+    print("donor can give blood")
+elif blood_group=="o-ab-":
+    print("donor can give blood")
+elif blood_group=="o-ab+":
+    print("donor can give blood")
+elif blood_group=="o+o-":
+    print("donor can't give blood")
+elif blood_group=="o+o+":
+    print("donor can give blood")
+elif blood_group=="o+a+":
+    print("donor can't give blood")
+elif blood_group=="o+a-":
+    print("donor can't give blood")
+elif blood_group=="o+b-":
+    print("donor can't give blood")
+elif blood_group=="o+b+":
+    print("donor can give blood")
+elif blood_group=="o+ab-":
+    print("donor can't give blood")
+elif blood_group=="o+ab+":
+    print("donor can give blood")
+elif blood_group=="a-o-":
+    print("donor can't give blood")
+elif blood_group=="a-o-":
+    print("donor can't give blood")
+elif blood_group=="a-a-":
+    print("donor can give blood")
+elif blood_group=="a-a+":
+    print("donor can give blood")
+elif blood_group=="a-b-":
+    print("donor can't give blood")
+elif blood_group=="a-b+":
+    print("donor can't give blood")
+elif blood_group=="a-ab-":
+    print("donor can give blood")
+elif blood_group=="a-ab+":
+    print("donor can give blood")
+elif blood_group=="a+o-":
+    print("donor can't give blood")
+elif blood_group=="a+o":
+    print("donor can't give blood")
+elif blood_group=="a+a-":
+    print("donor can't give blood")
+elif blood_group=="a+a":
+    print("donor can give blood")
+elif blood_group=="a+b-":
+    print("donor can't give blood")
+elif blood_group=="a+b":
+    print("donor can't give blood")
+elif blood_group=="a+ab-":
+    print("donor can't give blood")
+elif blood_group=="a+ab":
+    print("donor can't give blood")
+elif blood_group=="b-o-":
+    print("donor can't give blood")
+elif blood_group=="b-o+":
+    print("donor can't give blood")
+elif blood_group=="b-a-":
+    print("donor can't give blood")
+elif blood_group=="b-a+":
+    print("donor can't give blood")
+elif blood_group=="b-b-":
+    print("donor can give blood")
+elif blood_group=="b-b":
+    print("donor can give blood")
+elif blood_group=="b-ab-":
+    print("donor can give blood")
+elif blood_group=="b-ab+":
+    print("donor can give blood")
+elif blood_group=="b+o-":
+    print("donor can't give blood")
+elif blood_group=="b+o+":
+    print("donor can't give blood")
+elif blood_group=="b+a-":
+    print("donor can't give blood")
+elif blood_group=="b+a+":
+    print("donor can't give blood")
+elif blood_group=="b+b-":
+    print("donor can't give blood")
+elif blood_group=="b+b+":
+    print("donor can give blood")
+elif blood_group=="b+ab-":
+    print("donor can't give blood")
+elif blood_group=="b+ab+":
+    print("donor can give blood")
+elif blood_group=="ab-o-":
+    print("donor can't give blood")
+elif blood_group=="ab-o+":
+    print("donor can't give blood")
+elif blood_group=="ab-a-":
+    print("donor can't give blood")
+elif blood_group=="ab-a+":
+    print("donor can't give blood")
+elif blood_group=="ab-b-":
+    print("donor can't give blood")
+elif blood_group=="ab-b+":
+    print("donor can't give blood")
+elif blood_group=="ab-ab-":
+    print("donor can't give blood")
+elif blood_group=="ab+o-":
+    print("donor can't give blood")
+elif blood_group=="ab+o+":
+    print("donor can't give blood")
+elif blood_group=="ab+a-":
+    print("donor can't give blood")
+elif blood_group=="ab+a+":
+    print("donor can't give blood")
+elif blood_group=="ab+b-":
+    print("donor can't give blood")
+elif blood_group=="ab+b+":
+    print("donor can't give blood")
+elif blood_group=="ab+ab-":
+    print("donor can't give blood")
+else:
+    blood_group=="ab+ab+"
+    print("donor can give blood")
