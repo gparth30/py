8a461a97432fff3885fce9bc33e12e981d765b78
Parth Galoliya <96252142+gparth30@users.noreply.github.com>
2021-12-24 10:18:34 +0530

    using ifelif method find zodiac and symbol

---
+++ zodiac.py
@@ -0,0 +1,67 @@
+date=str(input("Enter Your Birth Date.:"))
+month=str(input("Enter Your Birth Month.:"))
+dob=date+month
+print(f"D.O.B={dob}")
+if dob in ['21March','22March','23March','24March','25March','26March','27March','28March','29March','30March','31March','1April','2April','3April','4April','5April','6April','7April','8April','9April','10April','11April','12April','13April','14April','15April','16April','17April','18April','19April']:
+    print('''
+    Your Zodiac Sign Is Aries
+    Your Zodiac Symbol Is The Ram
+    ''')
+elif dob in ['20April','21April','22April','23April','24April','25April','26April','27April','28April','29April','30April','1May','2May','3May','4May','5May','6May','7May','8May','9May','10May','11May','12May','13May','14May','15May','16May','17May','18May','19May','20May']:
+    print('''
+    Your Zodiac Sign Is Taurus
+    Your Zodiac Symbol Is The Bull
+    ''')
+elif dob in ['21May','22May','23May','24May','25May','26May','27May','28May','29May','30May','31May','1June','2June','3June','4June','5June','6June','7June','8June','9June','10June','11June','12June','13June','14June','15June','16June','17June','18June','19June','20June']:
+    print('''
+    Your Zodiac Sign Is Gemini
+    Your Zodiac Symbol Is The Twins
+    ''')
+elif  dob in ['21June','22June','23June','24June','25June','26June','27June','28June','29June','30June','1July','2July','3July','4July','5July','6July','7July','8July','9July','10July','11July','12July','13July','14July','15July','16July','17July','18July','19July','20July','21July','22July']:
+    print('''
+    Your Zodiac Sign Is Cancer
+    Your Zodiac Symbol Is The Crab
+    ''')
+elif dob in ['23July','24July','25July','26July','27July','28July','29July','30July','31July''1August','2August','3August','4August','5August','6August','7August','8August','9August','10August','11August','12August','13August','14August','15August','16August','17August','18August','19August','20August','21August','22August']:
+    print('''
+    Your Zodiac Sign Is Leo
+    Your Zodiac Symbol Is The Lion
+    ''')
+elif dob in ['23August','24August','25August','26August','27August','28August','29August','30August','31August''1September','2September','3September','4September','5September','6September','7September','8September','9September','10September','11September','12September','13September','14September','15September','16September','17September','18September','19September','20September','21September','22September']:
+    print('''
+    Your Zodiac Sign Is Virgo
+    Your Zodiac Symbol Is The Virgin
+    ''')
+elif dob in['23September','24September','25September','26September','27September','28September','29September','30September','1Octomber','2Octomber','3Octomber','4Octomber','5Octomber','6Octomber','7Octomber','8Octomber','9Octomber','10Octomber','11Octomber','12Octomber','13Octomber','14Octomber','15Octomber','16Octomber','17Octomber','18Octomber','19Octomber','20Octomber''21Octomber','22Octomber']:
+    print('''
+    Your Zodiac Sign Is Libra
+    Your Zodiac Symbol Is The Scales
+    ''')
+elif dob in ['23Octomber','24Octomber','25Octomber','26Octomber','27Octomber','28Octomber','29Octomber','30Octomber','31Octomber','1November','2November','3November','4November','5November','6November','7November','8November','9November','10November','11November','12November','13November','14November','15November','16November','17November','18November','19November','20November','21November']:
+    print('''
+    Your Zodiac Sign Is Scorpio
+    Your Zodiac Symbol Is The Scorpion
+    ''')
+elif dob in ['22November','23November','24November','25November','26November','27November','28November','29November','30November','1December','2December','3December','4December','5December','6December','7December','8December','9December','10December','11December','12December','13December','14December','15December','16December','17December','18December','19December','20December','21December']:
+    print('''
+    Your Zodiac Sign Is Sagittarius
+    Your Zodiac Symbol Is The Archer
+    ''')
+elif dob in ['22December','23December','24December','25December','26December','27December','28December','29December','30December','31December','1January','2January','3January','4January','5January','6January','7January','8January','9January','10January','11January','12January','13January','14January','15January','16January','17January','18January','19January']:
+    print('''
+    Your Zodiac Sign Is Capricorn
+    Your Zodiac Symbol Is The Goat
+    ''')
+elif dob in ['21January','22January','23January','24January','25January','26January','27January','28January','29January','30January','31January','1February','2February','3February','4February','5February','6February','7February','8February','9February','10February','11February','12February','13February','14February','15February','16February','17February','18February']:
+    print('''
+    Your Zodiac Sign Is Aquarius
+    Your Zodiac Symbol Is The Water Bearer
+    ''')
+else:
+    dob in ['19February','20February','21February','22February','23February','24February','25February','26February','27February','28February','29February','1March','2March','3March','4March','5March','6March','7March','8March','9March','10March','11March','12March','13March','14March','15March','16March','17March','18March','19March','20March']
+    print('''
+    Your Zodiac Sign Is Pisces
+    Your Zodiac Symbol Is The Water Fishes
+    ''')
+
+print("Have A Good Day")
