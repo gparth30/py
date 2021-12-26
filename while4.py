710620b870e4de24a401c0b53137f14a019e7ca2
Parth Galoliya <96252142+gparth30@users.noreply.github.com>
2021-12-26 22:18:09 +0530

    using while loop make hexagonal series

---
+++ while4.py
@@ -0,0 +1,8 @@
+#write a program to print following series (hexagonal number)
+#1, 6, 15, 28, 45, 66, 91, 120, 153, 190, 231 .. 1000
+
+number=1
+while number<23:
+    add=2*(number*number)-number
+    print(add,end=' ')
+    number=number+1
