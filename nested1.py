e43365c9dc0f46214a1f4220be097887e1086a54
Parth Galoliya <96252142+gparth30@users.noreply.github.com>
2021-12-27 20:39:15 +0530

    draw *

---
+++ nested1.py
@@ -0,0 +1,15 @@
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+'''
+row=1
+while row<=5: #outer loop 3
+    column=1 #1
+    while column<=row: #inner loop 1<=5
+        print("*",end=' ') #action
+        column=column+1 #count
+    print("")
+    row=row+1
