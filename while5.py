99e37a5b765af10d6eb8b6078a66510450ede4b7
Parth Galoliya <96252142+gparth30@users.noreply.github.com>
2021-12-27 12:27:06 +0530

    program of findout power

---
+++ while5.py
@@ -0,0 +1,6 @@
+#write a program to findout power of given number using given exponen
+base=int(input("Enter Base.:"))
+power=int(input("Enter Power.:"))
+total=base**power
+print(f"Total= {total}")
+
